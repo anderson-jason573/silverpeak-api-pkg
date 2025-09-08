16d00a49e618a86311bdf9e0bc81598fdbd87ade
Jason Anderson <anderson.jason573@gmail.com>
2025-09-08 15:59:32 -0600

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -5,15 +5,12 @@ This module initializes the 'api' package, when it is imported.
 It imports all of the functions from the modules in the api 
 package, so they can be called by using 'api.<function>'.
 
+It also imports the requests, json and sys packages.
+
 ***************************************************************
 """
 import requests
 import json
 import sys
-from .login import OrchLogin
-from .yamlConversion import readYAML
-from .preconfigOrch import preconfigUpload
-from .spcustomFile import spcustomCreate
-from .applianceInfo import applianceINFO
 from .get_Orchestrator_Hostname import OrchHostname
 #end
