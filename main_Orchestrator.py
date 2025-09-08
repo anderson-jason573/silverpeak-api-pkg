186c4c61bec3caa3e621c1ce11869450adcf9235
Jason Anderson <anderson.jason573@gmail.com>
2025-09-08 09:23:12 -0600

    Error handling completed and tested

--- main_Orchestrator.py
+++ main_Orchestrator.py
@@ -3,12 +3,16 @@ import os
 from dotenv import find_dotenv, load_dotenv
 
 #load Orchestrator URL and API Key from .env file
-dotenv_path = find_dotenv()     # Finds path to .env file
-load_dotenv(dotenv_path)        # loads .env file
+env_path = '.env'                                                               # Or specify a different path
+if not os.path.exists(env_path):                                                # Error Handling if .env cannot be found       
+    raise FileNotFoundError(f"The .env file was not found at: {env_path}")
+else:
+    load_dotenv(dotenv_path=env_path)
+    print("\nEnvironment variables loaded successfully.")
 
 # Set Orchestrator FQDN/IP and API Key via from .env file
-orch_url = os.environ.get("ORCH_URL")              # reads .env file and sets 'orch_url' variable
-orch_api_key = os.environ.get("ORCH_API_KEY")      # reads .env file and sets 'orch_api_key' variable
+orch_url = os.environ.get("ORCH_URL")                                           # reads .env file and sets 'orch_url' variable
+orch_api_key = os.environ.get("ORCH_API_KEY")                                   # reads .env file and sets 'orch_api_key' variable
 
 """
 #Pass Orchestrator url and headers for API Call to desired module/function.
@@ -17,6 +21,7 @@ orch_api_key = os.environ.get("ORCH_API_KEY")      # reads .env file and sets 'o
 #Example:
 
 apiCall = api.<desired module/function>(orch_url, headers)
+
 """
-apiCall = api.OrchHostname(orch_url, orch_api_key)  # passes url and api key to 'OrchHostname function' in 'GET_Orchestrator_Hostname.py' module
+apiCall = api.OrchHostname(orch_url, orch_api_key)                              # passes url and api key to 'OrchHostname function' in 'GET_Orchestrator_Hostname.py' module
 
